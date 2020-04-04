1d15cb47c0d52b2e8aeaf06c1c92bea858f1bf07
codepad <codepad@anima.d250.hu>
2020-04-05 00:41:29 +0200

    0.0.4

---
+++ anima-modules/app/navigation.vue
@@ -0,0 +1,44 @@
+<template>
+    <v-app-bar app>
+        <v-toolbar-items>
+            <v-btn text to="/">HOME</v-btn>
+            <v-btn text to="/boilerplate">Boilerplate</v-btn>
+            <v-btn text to="/vue">Vue</v-btn>
+            <v-btn text to="/vuetify">Vuetify</v-btn>
+            <v-btn text to="/session">Session</v-btn>
+            <v-btn text to="/socket-io">Socket.io</v-btn>
+            <v-btn text to="/payment">Payment</v-btn>
+            <v-btn text to="/hello_login">##&en Login ##&hu Bejelentkezés ##</v-btn>
+            <v-btn v-on:click="$root.login_dialog.open();">##&en Login Dialog ##&hu login Dialogus ##</v-btn>
+            <language_selector></language_selector>
+          	<v-btn v-if="$store.state.server.session.is_admin" text @click="open_admin()">Admin</v-btn>
+            <v-btn v-if="debug" text @click="open_editor()">Editor</v-btn>
+        </v-toolbar-items>
+    </v-app-bar>
+</template>
+
+<script>
+import language_selector from "@/components/LanguageSelector.vue";
+
+export default {
+    name: "navigation",
+    data: function() {
+        return {
+        	debug: ß.DEBUG, 
+        };
+    },
+    components: {
+        language_selector
+    },
+    methods:{
+      open_editor: function () {   
+          window.open('https://' + ß.HOSTNAME + ':9001', "_blank");    
+      },
+      open_admin: function () {   
+          window.open('https://' + ß.HOSTNAME + '/admin', "_blank");    
+      }
+    }
+};
+</script>
+
+<style></style>
