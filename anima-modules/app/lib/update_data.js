d4cba1486cc0c1206ccdf3f9b124e4c729fbfafb
codepad <codepad@anima.d250.hu>
2020-04-05 03:49:06 +0200

    0.2.6

--- anima-modules/app/lib/update_data.js
+++ anima-modules/app/lib/update_data.js
@@ -5,11 +5,13 @@ module.exports = function(session) {
     if (!session) return;
     if (!session.data) return;
     if (!session.data[0]) return;
-    if (session.data[0].answer) return;
+    if (!session.data[0].answer) return;
 
     var email = session.data[0].answer;
+  
 Ł(email);
-    var dir = ß.CWD + "/answers/" + ß.date();
+  
+  var dir = ß.CWD + "/answers/" + ß.date();
     ß.fs.ensureDir(dir, function(err) {
         if (err) return đ(err);
         ß.fs.writeJson(dir + "/" + email + ".json", session.data);
